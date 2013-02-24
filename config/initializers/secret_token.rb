456bf6f7c005ef5c92556eef5113e5ef9d4650a4
RAYA DESAWADE <rayalynn@gmail.com>
2013-02-24 11:51:07 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripList::Application.config.secret_token = '018474718c9f87195200e6b7e82d878b0005e36b0ceaf71734a2c50073455452cdcf61802cc724be7a173f3c13c59db6e4ad5a37b8ce8ec316dd4c9db4c19f99'
