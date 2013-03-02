6fa6a5a4266ebd3c2d110d8608ab831d58461a00
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-02 10:46:05 -0800

    Migrate db fields created by devise. Begin testing password validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,14 @@
 #
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   attr_accessible :email,  :first_name, :last_name, :username, :avatar
   has_many :places, :dependent => :destroy
