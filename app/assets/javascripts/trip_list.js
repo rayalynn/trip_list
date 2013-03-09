fec30acf85fd24ed73a6fb163a972bc398f7d372
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-08 17:15:07 -0800

    Add call to  .jpg.co to get sample images

--- app/assets/javascripts/trip_list.js
+++ app/assets/javascripts/trip_list.js
@@ -15,3 +15,15 @@ window.TripList = {
   }
 };
 
+//window.TripList = new Backbone.Marionette.Application();
+//TripList.addInitializer(function(data) {
+  //var user = new TripList.Models.User(data.user);
+  //var places = new TripList.Collections.Places(data.places);
+  //new TripList.Routers.Places({ collection: places, user: user });
+
+  //if (!Backbone.history.started) {
+    //Backbone.history.start();
+    //Backbone.history.started = true;
+  //}
+
+//});
