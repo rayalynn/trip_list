96ac160fd1ecdaa941a018f543955920b1cec9d2
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-08 18:09:44 -0800

    Switch to Marionette instance of app. Add Masonary for future use.

--- app/assets/javascripts/trip_list.js
+++ app/assets/javascripts/trip_list.js
@@ -1,29 +1,21 @@
-window.TripList = {
+window.TripList = new Backbone.Marionette.Application();
+_.extend(TripList, {
   Models: {},
   Collections: {},
-  Views: {},
+  Controllers: {},
   Routers: {},
-  initialize: function(data) {
-    var user = new TripList.Models.User(data.user);
-    var places = new TripList.Collections.Places(data.places);
-    new TripList.Routers.Places({ collection: places, user: user });
+  Layouts: {},
+  Views: {}
+});
 
-    if (!Backbone.history.started) {
-      Backbone.history.start();
-      Backbone.history.started = true;
-    }
+TripList.addInitializer(function(data) {
+  var user = new TripList.Models.User(data.user);
+  var places = new TripList.Collections.Places(data.places);
+  new TripList.Routers.Places({ collection: places, user: user });
+
+  if (!Backbone.history.started) {
+    Backbone.history.start();
+    Backbone.history.started = true;
   }
-};
 
-//window.TripList = new Backbone.Marionette.Application();
-//TripList.addInitializer(function(data) {
-  //var user = new TripList.Models.User(data.user);
-  //var places = new TripList.Collections.Places(data.places);
-  //new TripList.Routers.Places({ collection: places, user: user });
-
-  //if (!Backbone.history.started) {
-    //Backbone.history.start();
-    //Backbone.history.started = true;
-  //}
-
-//});
+});
