07af2e22a4457c3f4f3d4945f77dc0694a35bb59
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-10 22:12:10 -0700

    Fix sidebar events after accidentally deleting them.

--- app/assets/javascripts/views/layouts/sidebar_view.js
+++ app/assets/javascripts/views/layouts/sidebar_view.js
@@ -4,6 +4,23 @@ TripList.Views.Sidebar = Backbone.Marionette.ItemView.extend({
 
   template: 'layouts/sidebar',
 
+  events: {
+    'click .visited': 'showVisitedPage',
+    'click .toVisit': 'showToVisitPage',
+    'click .addNewPlace': 'addNewPlace'
+  },
+
+  showVisitedPage: function() {
+    TripList.vent.trigger('showVisitedPage');
+  },
+  
+  showToVisitPage: function() {
+    TripList.vent.trigger('showToVisitPage');
+  },
+
+  addNewPlace: function() {
+    TripList.vent.trigger('addNewPlace');
+  }
 });
 
 
