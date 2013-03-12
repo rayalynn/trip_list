dc2ac1eb93bef111a6aa6bb03175a4ad42a06e70
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-12 11:44:31 -0700

    Accidentally overwrote my refactor. Redid it.

--- app/assets/javascripts/views/places/places_index.js
+++ app/assets/javascripts/views/places/places_index.js
@@ -1,6 +1,10 @@
-TripList.Views.Places = Backbone.Marionette.CollectionView.extend({
-  itemView: TripList.Views.PlaceItemView, 
+/* Main view for all places. Shows Unvisited and Visted places.
+ * Masonry init code from:
+ *   http://www.maurizioconventi.com/2012/06/19/responsive-example-integrating-twitter-bootstrap-and-jquery-masonry/
+ */
+TripList.Views.PlacesIndex = Backbone.Marionette.CollectionView.extend({
 
+  itemView: TripList.Views.PlaceItemView, 
   template: JST['places/index'],
   el: '.main',
   url: '/places',
@@ -9,83 +13,51 @@ TripList.Views.Places = Backbone.Marionette.CollectionView.extend({
   initialize: function() {
     var self = this;
 
-    _.bindAll(this, 'render', 'appendPlace', 'init_masonry',
-             'showVisitedPlaces', 'appendNewItem');
-
     TripList.vent.on('showVisitedPlaces', function() {
-      console.log("in show visited places on main");
       self.showVisitedPlaces();
     });
-
-    this.collection.bind('add', this.appendNewItem);
-    this.collection.bind('remove', this.render);
   },
 
   render: function() {
-    console.log("Calling main render function");
     this.showPlacesToVisit();
     this.init_masonry();
     return this;
   },
 
+  showPlacesToVisit: function() {
+    $(this.el).html('');
+    var incompleteItems = this.collection.remainingPlaces();
+    _(incompleteItems).each(function(item) {
+      this.appendPlace(item);
+    }, this);
+  },
+  
+  //Create new view for each individual place and append to main
+  appendPlace: function(item) {
+    var placeItemView = new TripList.Views.PlaceItemView({
+      model: item,
+    });
+    this.$el.append(placeItemView.render());
+  },
+
+  //Load up masonry to display photos Pinterest style
   init_masonry: function() {
     var $container = $(this.el);
     var gutter = 40;
     var min_width = 200;
     $container.imagesLoaded( function(){
-        $container.masonry({
-            itemSelector : '.main-box',
-            gutterWidth: gutter,
-            isAnimated: true,
-              columnWidth: function( containerWidth ) {
-                var num_of_boxes = (containerWidth/min_width | 0);
-
-                var box_width = (((containerWidth - (num_of_boxes-1)*gutter)/num_of_boxes) | 0) ;
-
-                if (containerWidth < min_width) {
-                    box_width = containerWidth;
-                }
-
-                $('.main-box').width(box_width);
-
-                return box_width;
-              }
-        });
+      $container.masonry({
+        itemSelector : '.main-box',
+        gutterWidth: gutter,
+        isAnimated: true,
+        columnWidth: function( containerWidth ) {
+        var num_of_boxes = (containerWidth/min_width | 0);
+        var box_width = (((containerWidth - (num_of_boxes-1)*gutter)/num_of_boxes) | 0) ;
+        if (containerWidth < min_width) { box_width = containerWidth; }
+          $('.main-box').width(box_width);
+          return box_width;
+        }
+      });
     });
   },
-
-  appendPlace: function(item) {
-    var placeItemView = new TripList.Views.PlaceItemView({
-      model: item,
-    });
-    var renderRetval = placeItemView.render()
-    this.$el.append(renderRetval);
-  },
-
-  showPlacesToVisit: function() {
-    $(this.el).html('');
-    var incompleteItems = this.collection.remainingPlaces();
-    console.log("show----");
-    debugger;
-    _(incompleteItems).each(function(item) {
-      this.appendPlace(item);
-    }, this);
-
-  },
-
-  showVisitedPlaces: function() {
-    console.log("Showing complete places");
-    $(this.el).html('');
-    var completedItems = this.collection.completedPlaces();
-    debugger;
-    _(completedItems).each(function(item) {
-      this.appendPlace(item);
-    }, this);
-  },
-
-  appendNewItem: function() {
-    console.log("This collection updated.");
-    var collection = this.collection.fetch();
-  }
-
 });
